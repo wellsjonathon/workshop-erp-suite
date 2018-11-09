c2ecb2bc89ea9462f6ac3df16c4a1498b8e4b489
wellsjonathon <wellsjonathonr@gmail.com>
2018-11-08 18:03:02 -0600

    Initializing client

---
+++ client/shop-client/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <NavTop id="nav__top"/>
+    <NavSide id="nav__side"/>
+    <div id="content">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NavTop from './components/Nav/Top.vue'
+import NavSide from './components/Nav/Side.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    NavTop,
+    NavSide
+  }
+}
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+#nav__top {
+  height: 50px;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
+#nav__side {
+  height: 100%;
+  width: 60px;
+  margin: 0;
+  padding: 0;
+  position: fixed;
+  top: 50px;
+  left: 0;
+  z-index: 99;
+}
+#content {
+  height: 100%;
+  width: 100%;
+  top: 50px;
+  left: 60px;
+  position: fixed;
+  background-color: aqua;
+}
+</style>
---
+++ client/shop-client/src/components/Nav/Side.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav__side">
+    <div id="nav__side__toggle">
+      +
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavSide'
+}
+</script>
+
+<style scoped>
+.nav__side {
+  width: 100%;
+  background-color: #F6F6F6;
+  box-shadow: 2px 2px 4px rgba(1,1,1,0.1),
+              1px 1px 16px rgba(1,1,1,0.075);
+}
+#nav__side__toggle {
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  right: 0;
+  top: 45%;
+  border-radius: 12px;
+  box-shadow: 0px 0px 4px rgba(1, 1, 1, 0.2);
+}
+</style>
+
