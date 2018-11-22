5754e110a06dfa02e7b293a55a71c0d03acefe78
wellsjonathon <wellsjonathonr@gmail.com>
2018-11-21 23:12:55 -0600

    Working EF context and endpoint

--- server/ERP/ERP.API/Startup.cs
+++ server/ERP/ERP.API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using ERP.Repositories.Context;
 
 namespace ERP.API
 {
@@ -25,6 +27,10 @@ namespace ERP.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VendorContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Is this line needed if we're not using RazorPages?
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
