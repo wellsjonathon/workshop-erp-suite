4fc13c327ac923b8e481ca3e10b73071c8d0aa8d
wellsjonathon <wellsjonathonr@gmail.com>
2018-11-21 19:09:27 -0600

    Tying together projects

---
+++ server/ERP/ERP.Repositories/Context/WorkorderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ERP.Models;
+
+namespace ERP.Repositories.Context
+{
+    public class WorkorderContext : DbContext
+    {
+        public WorkorderContext(DbContextOptions<WorkorderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Workorder> Workorders { get; set; }
+    }
+}
