bf348ae381dd262a430e641da7bae6c18711048d
wellsjonathon <wellsjonathonr@gmail.com>
2019-02-01 08:48:28 -0600

    Cleaned up a few lines

--- client/shop-client/src/components/Inventory/Inventory.vue
+++ client/shop-client/src/components/Inventory/Inventory.vue
@@ -5,7 +5,7 @@
         Inventory
       </div>
     </div>
-    <div>
+    <div class="content-area">
       <!-- 
       <ul>
         <li v-for="material in materials" :key="material.id">
@@ -18,30 +18,29 @@
         </li>
       </ul>
       -->
-    </div>
     <!-- Code to be removed later --> 
-    <table style="width:100%">
-  <tr>
-    <th>ID</th>
-    <th>Name</th>
-    <th>Category </th> 
-    <th>Type</th>
-    <th>Quantity In Stock </th>
-    <th>Details</th>
-  </tr>
-  <tbody>
-    </tbody>
-  <tr v-for="material in materials" :key="material.id">
-    <!-- <td>{{material.id}}</td> -->
-    <td>{{material.id}}</td>
-    <td>{{material.name}}</td>
-    <td>{{material.MaterialCategory}}</td>
-    <td>{{material.MaterialType}}</td>
-    <td>{{material.quantityInStock}}</td>
-    <td>{{material.details}}</td>
-  </tr>
-  
-</table>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Category </th> 
+          <th>Type</th>
+          <th>Quantity In Stock </th>
+          <th>Details</th>
+        </tr>
+        <tbody>
+          </tbody>
+        <tr v-for="material in materials" :key="material.id">
+          <!-- <td>{{material.id}}</td> -->
+          <td>{{material.id}}</td>
+          <td>{{material.name}}</td>
+          <td>{{material.MaterialCategory}}</td>
+          <td>{{material.MaterialType}}</td>
+          <td>{{material.quantityInStock}}</td>
+          <td>{{material.details}}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 
 </template>
@@ -67,38 +66,50 @@ export default {
 
 <style scoped>
 .page__heading {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   padding: 21px 28px;
-  font-size: 4rem;
+  border-bottom: 1px solid #DDDDDD; /* Maybe reconsider shade later*/
+  background-color: #FFFFFF;
+  /* font-weight: 700; */
+  /* box-shadow: 0px 2px 4px -1px rgba(1,1,1,0.125),
+              0px 1px 16px -1px rgba(1,1,1,0.075); */
+}
+.page__heading__title {
+  /* padding: 21px 28px; */
+  font-size: 3.5rem;
+}
+/* Code to be removed later */
+.content-area {
+  padding: 14px;
 }
 
-/* Code to be removed later */
-
-
 table {
-
+  width: 100%;
   margin: 25px auto;
   border-collapse: collapse;
   border: 1px solid #eee;
   border-bottom: 2px solid #425B72;
 }
-  tr:hover {
-      background: #f4f4f4;
-  }
-  td {
-        color: #560;
-        font-size: 22px;
-      }
-  th, td {
-    color: #999;
-    border: 1px solid #eee;
-    padding: 12px 35px;
-    border-collapse: collapse;
-  }
-  th {
-    background: #425B71;
-    color: #fff;
-    text-transform: uppercase;
-    font-size: 12px;
-  }
+tr, td {
+  font-size: 1.6rem;
+  color: #560;
+}
+tr:hover {
+    background: #f4f4f4;
+}
+th, td {
+  color: #999;
+  border: 1px solid #eee;
+  padding: 12px 35px;
+  border-collapse: collapse;
+}
+th {
+  background: #425B71;
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 12px;
+}
 
 </style>
