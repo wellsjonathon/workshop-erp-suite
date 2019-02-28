a7ac5125713e8efa87c8b199590356e6dcc65d2d
Konstantin Kharitonov <konstantinkharitoovpro@gmail.com>
2019-02-28 13:25:16 -0600

    Removed duplicate files.

--- server/ERP/ERP.Models/Workorders/WorkorderNote.cs
+++ server/ERP/ERP.Models/Workorders/WorkorderNote.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ERP.Models
+namespace ERP.Models.Workorders
 {
     public class WorkorderNote
     {
