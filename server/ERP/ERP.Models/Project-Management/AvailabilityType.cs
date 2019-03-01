14f27a985fafbf1d395b9ce1535f839394a568e8
Konstantin Kharitonov <konstantinkharitoovpro@gmail.com>
2019-02-28 18:40:26 -0600

    Created models for project management section.

---
+++ server/ERP/ERP.Models/Project-Management/AvailabilityType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP.Models.Project_Management
+{
+    public class AvailabilityType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string DefaultColour { get; set; }
+    }
+}
---
+++ server/ERP/ERP.Models/Project-Management/Availablity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP.Models.Project_Management
+{
+    public class Availablity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public string Notes { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public AvailabilityType AvailabilityTypeId { get; set; }
+    }
+}
